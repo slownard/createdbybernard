f196a6a44ec3747a892eb25b23fcc1a92a78c3c7
Bernard Legaspi <blegaspi26@gmail.com>
2023-11-08 16:19:17 -0500

    cleaning it up a little

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          IM JUST TRYING TO FIGURE THIS THING OUT BECAUSE I FORGOT SO MUCH!
-        </a>
-        <a>
-          UPDATE: I THINK I JUST GOT IT.
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           IM JUST TRYING TO FIGURE THIS THING OUT BECAUSE I FORGOT SO MUCH!
+//         </a>
+//         <a>
+//           UPDATE: I THINK I JUST GOT IT.
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <h1>
+                    Bernard Legaspi
+                </h1>
+
+                <a>
+                    Software Developer
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
